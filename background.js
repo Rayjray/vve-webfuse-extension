4b817dbf97747eaf11ea1598b0e9369ba897cb2e
Rayjray <info@nutmeg-projects.com>
2026-03-05 15:20:47 +0100

    fix: async sendResponse in background.js

--- background.js
+++ background.js
@@ -1,11 +1,12 @@
-// Background service worker
-// Listens for session info requests from the popup
-
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
   if (message.type === 'getSessionInfo') {
-    browser.webfuseSession.getSessionInfo().then((info) => {
-      sendResponse({ sessionInfo: info });
-    });
-    return true; // Keep message channel open for async response
+    browser.webfuseSession.getSessionInfo()
+      .then((info) => {
+        sendResponse({ sessionInfo: info });
+      })
+      .catch((err) => {
+        sendResponse({ sessionInfo: null, error: err.message });
+      });
+    return true;
   }
 });
